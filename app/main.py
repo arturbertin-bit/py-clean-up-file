f13f824b9ae160dbbc830b7475342a69b2b88354
Artur Bertin <arturbertin2006@gmail.com>
2026-01-09 12:14:56 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import os
-import traceback
 from os.path import exists
 
 
@@ -7,7 +6,7 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> "CleanUpFile":
         return self
 
     def __exit__(self, exc_type: None, exc_val: None, exc_tb: None) -> None:
